faadbec55e42d801ca750a6e003fad4456fd98dd
RickCarr <rick.carr@telus.com>
2022-11-24 16:24:32 -0800

    modular-ized index for user interface, added test dir and mocha/chai enabled test file.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,18 @@
 const request = require('request');
 
-const breed = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
 
-  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('body:', body); // Print the HTML for the Google homepage.
-  // console.log(typeof body);
-  if (error) {
-    return console.log(`The site ${error.hostname} is not reachable.`);
-  }
+const fetchBreedDescription = (param1, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${param1}`, (error, response, body) => {
 
-  const data = JSON.parse(body);
-  data[0] ? console.log(data[0].description) : console.log(`The result of your breed search for a ${breed} is`, error, '(try a different breed.)');
-  //  console.log(error);
-});
+    if (error) {
+      return callback(`The site ${error.hostname} is not reachable.`, null);
+    }
+
+    const data = JSON.parse(body);
+
+    data[0] ? callback(null, data[0].description) : callback(error, `The result of your breed search for a ${param1} is absent from this index.\nPlease try a different breed.`);
+  });
+};
+
+module.exports = { fetchBreedDescription };
